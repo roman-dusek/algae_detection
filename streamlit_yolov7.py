79fcb9b72165945777871bec0b5dd93b745dc353
Roman Dusek <roman.dusek2@mallgroup.com>
2022-12-10 11:23:05 +0100

    fixes

--- streamlit_yolov7.py
+++ streamlit_yolov7.py
@@ -108,7 +108,7 @@ if __name__=='__main__':
     #INPUTS for YOLOV7
     img_size=640
     path_yolov7_weights="weights/best.pt"
-    path_img_i="https://raw.githubusercontent.com/stevensmiley1989/STREAMLIT_YOLOV7/main/test_images/dinobryon_3.jpg"
+    path_img_i="test_images/dinobryon_3.jpg"
     #INPUTS for webapp
     app.capt="Initial Image"
     app.new_yolo_model(img_size,path_yolov7_weights,path_img_i)
